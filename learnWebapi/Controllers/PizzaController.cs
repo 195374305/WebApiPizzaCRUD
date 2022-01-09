ea2783447ae98e13a9115e7252e80decedcafc75
195374305 <195374305@qq.com>
2022-01-09 21:01:07 +0800

    添加项目文件。

---
+++ learnWebapi/Controllers/PizzaController.cs
@@ -0,0 +1,75 @@
+﻿using learnWebapi.Model;
+using learnWebapi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace learnWebapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController()
+        {
+        }
+
+        // GET all action
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() 
+        {
+           return PizzaService.GetAll();
+        }
+
+
+        // GET by Id action
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if (pizza == null)
+                return NotFound();
+
+            return pizza;
+        }
+
+
+        // POST action
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {
+            PizzaService.Add(pizza);
+            return CreatedAtAction("Create", new { id = pizza.Id }, pizza);//生成响应头
+
+        }
+
+        // PUT action
+        [HttpPut]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            if (id != pizza.Id)
+                return BadRequest();
+
+            var existingPizza = PizzaService.Get(id);
+            if (existingPizza is null)
+                return NotFound();
+
+            PizzaService.Update(pizza);
+
+            return NoContent();
+        }
+
+        // DELETE action
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if (pizza is null)
+                return NotFound();
+
+            PizzaService.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
